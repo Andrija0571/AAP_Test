7c222672955e89c98e9bc64f4aa4d561769577ba
Andrija0571 <56803761+Andrija0571@users.noreply.github.com>
2019-10-21 18:47:41 +0200

    Add files via upload

--- elementi/glavni.php
+++ elementi/glavni.php
@@ -127,7 +127,8 @@ class Glavni
 				echo file_get_contents($file);
 				break;
 			default:
-				header('Location: http://www.recepcija.hr');
+				//actually redirects away as it would be illegal call
+				header('Location: index.php?status=failed');
 				break;
 		}
 		die;
