a1779572be1958138a2ebe4c79933f76549dc923
Andrija0571 <56803761+Andrija0571@users.noreply.github.com>
2019-10-21 18:43:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
-//this is very simplified version of my current login module access
-//note: there is no password confirmation field (it is never used in login screens, only
-//in password change screens) - data in login forms is never sent in plain form but as hashes and cannot
-//be reloaded (unless ajax is used or data is stored in local storage)
-//since I always assume web app will be attacked, I prefer not to allow fast response to failed
-//login attempt
-//form error checking is demonstrated with input value length<3, same principle goes for password
-//checking $(#input1).val()!=$(#input2).val()
-//username 12345 will pass, any other input will not pass
-//not clicking a color (any) will fail
+//A simplified version of my current login module\
+//Notes: 
+//a) there is no password confirmation field (it is never used in login screens, only in password change screens) 
+//b) data in login forms is never sent in plain form but as hashes and cannot be reloaded
+//    (unless ajax is used or data is stored in local storage) 
+//c) I have added return to index.php but in general prefer not to allow fast response to failed login attempt
+//d) form error checking is demonstrated with input value length<3 
+//e) password match (for two fields) could be done: 
+//	$(#input1).val()!=$(#input2).val() 
+//Since there is no database in backend, username '12345' (with any password) will pass, any other input will return error
+//There is also a Captcha mockup in form of color picker: you must click on any color to proceed
+
 $provjera=17;
 $password_failed=false;
 while (list ($key,$val) = each ($_GET))
